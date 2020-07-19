a05c26f28cd552e46edec486d8f61f0ab50ca3b9
Shayak <44469461+shr0nin@users.noreply.github.com>
2020-07-19 21:04:31 +0530

    Create Mobile numeric keypad.cpp
    https://practice.geeksforgeeks.org/problems/mobile-numeric-keypad/0

---
+++ Mobile numeric keypad.cpp
@@ -0,0 +1,102 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define int long long
+
+vector<vector<int>> neb;
+
+void init() {
+    vector<int> v;
+    // 0
+    v.emplace_back(8);
+    neb.emplace_back(v);
+    // 1
+    v.clear();
+    v.emplace_back(2);
+    v.emplace_back(4);
+    neb.emplace_back(v);
+    // 2
+    v.clear();
+    v.emplace_back(1);
+    v.emplace_back(3);
+    v.emplace_back(5);
+    neb.emplace_back(v);
+    // 3
+    v.clear();
+    v.emplace_back(2);
+    v.emplace_back(6);
+    neb.emplace_back(v);
+    // 4
+    v.clear();
+    v.emplace_back(1);
+    v.emplace_back(5);
+    v.emplace_back(7);
+    neb.emplace_back(v);
+    // 5
+    v.clear();
+    v.emplace_back(2);
+    v.emplace_back(4);
+    v.emplace_back(6);
+    v.emplace_back(8);
+    neb.emplace_back(v);
+    // 6
+    v.clear();
+    v.emplace_back(3);
+    v.emplace_back(5);
+    v.emplace_back(9);
+    neb.emplace_back(v);
+    // 7
+    v.clear();
+    v.emplace_back(4);
+    v.emplace_back(8);
+    neb.emplace_back(v);
+    // 8
+    v.clear();
+    v.emplace_back(0);
+    v.emplace_back(5);
+    v.emplace_back(7);
+    v.emplace_back(9);
+    neb.emplace_back(v);
+    // 9
+    v.clear();
+    v.emplace_back(6);
+    v.emplace_back(8);
+    neb.emplace_back(v);
+}
+
+const int nax=32;
+vector<vector<int>> dp(nax, vector<int> (10));
+vector<int> pref(nax);
+
+void build() {
+    for(int j=0; j<10; ++j) {
+        dp[1][j]=1;
+    }
+    for(int i=2; i<nax; ++i) {
+        for(int j=0; j<10; ++j) {
+            dp[i][j]=dp[i-1][j];
+            for(int his: neb[j]) {
+                dp[i][j]+=dp[i-1][his];
+            }
+        }
+    }
+    for(int i=0; i<nax; ++i) {
+        for(int j=0; j<10; ++j) {
+            pref[i]+=dp[i][j];
+        }
+    }
+}
+ 
+signed main() {
+    init();
+    build();
+	int t;
+	scanf("%lld", &t);
+	while(t--) {
+	    int n;
+	    scanf("%lld", &n);
+	    printf("%lld\n", pref[n]);
+	}
+ 
+    return 0;
+}
