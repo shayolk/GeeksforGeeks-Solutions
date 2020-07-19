2eb5a24098a7db82a5aac34436b2727421ccc542
Shayak <44469461+shr0nin@users.noreply.github.com>
2020-07-19 11:41:41 +0530

    Add files via upload
    https://practice.geeksforgeeks.org/problems/nth-fibonacci-number/0

---
+++ Nth Fibonacci Number.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+const int M=1e9+7;
+ 
+int main() {
+  vector<int> fib(1001, 1);
+  for(int i=3; i<=1000; ++i) {
+      fib[i]=(fib[i-1]+fib[i-2])%M;
+  }
+  int t;
+  scanf("%d", &t);
+  while(t--) {
+      int n;
+      scanf("%d", &n);
+      printf("%d\n", fib[n]);
+  }
+ 
+    return 0;
+}
