eae1be5cfede987005e1f775bff7c18a5460b57b
Shayak <44469461+shr0nin@users.noreply.github.com>
2020-07-20 19:10:07 +0530

    Create Maximum Difference.cpp
    https://practice.geeksforgeeks.org/problems/maximum-difference/0

---
+++ Maximum Difference.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int t;
+    scanf("%d", &t);
+    while(t--) {
+        int n;
+        scanf("%d", &n);
+        int diff=0, x;
+        scanf("%d", &x);
+        int mn=x, mx=x;
+        for(int i=1; i<n; ++i) {
+            scanf("%d", &x);
+            if(x<mn) {
+                mn=x;
+            }
+            else {
+                mx=x;
+                diff=max(diff, mx-mn);
+            }
+        }
+        printf("%d\n", diff?diff:-1);
+    }
+    
+    return 0;
+}
