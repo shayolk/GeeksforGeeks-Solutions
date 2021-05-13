9d5b0f75ecfe17d67ba643e22dcacdf25ec4d22b
Shayak <44469461+shr0nin@users.noreply.github.com>
2021-05-13 20:35:39 +0530

    Create Searching a number.cpp
    https://practice.geeksforgeeks.org/problems/searching-a-number0324/1

---
+++ Searching a number.cpp
@@ -0,0 +1,12 @@
+//User function template for C++
+class Solution{
+public:
+	int search(int arr[], int n, int k) {
+	    for(int i=0; i<n; ++i) {
+	        if(arr[i]==k) {
+	            return i+1;
+	        }
+	    }
+	    return -1;
+	}
+};
