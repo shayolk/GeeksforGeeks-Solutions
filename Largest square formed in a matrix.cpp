f78eb90e4498a4cb831c662cfdf6a172884d980b
Shayak <44469461+shr0nin@users.noreply.github.com>
2021-08-15 18:52:23 +0530

    Update Largest square formed in a matrix.cpp
    https://practice.geeksforgeeks.org/problems/largest-square-formed-in-a-matrix0806/1

--- Largest square formed in a matrix.cpp
+++ Largest square formed in a matrix.cpp
@@ -1,35 +1,26 @@
-#include <bits/stdc++.h>
-using namespace std;
- 
-int main() {
-    int t;
-    scanf("%d", &t);
-    while(t--) {
-        int n,m;
-        scanf("%d%d", &n,&m);
-        vector<vector<int>> a(n, vector<int> (m)), dp(n, vector<int> (m));
+// User function Template for C++
+
+class Solution{
+public:
+    int maxSquare(int n, int m, vector<vector<int>> mat){
+        vector<vector<int>> dp=mat;
         int ans=0;
         for(int i=0; i<n; ++i) {
-            for(int j=0; j<m; ++j) {
-                scanf("%d", &a[i][j]);
-                if(!i || !j) {
-                    dp[i][j]=a[i][j];
-                }
-                if(a[i][j]) {
-                    ans=1;
-                }
-            }
+            if(mat[i][0]) ans=1;
+            dp[i][0]=mat[i][0];
+        }
+        for(int j=0; j<m; ++j) {
+            if(mat[0][j]) ans=1;
+            dp[0][j]=mat[0][j];
         }
         for(int i=1; i<n; ++i) {
             for(int j=1; j<m; ++j) {
-                if(a[i][j]) {
+                if(mat[i][j]) {
                     dp[i][j]=1+min({dp[i-1][j], dp[i][j-1], dp[i-1][j-1]});
                     ans=max(ans, dp[i][j]);
                 }
             }
         }
-        printf("%d\n", ans);
+        return ans;
     }
- 
-    return 0;
-}
+};
