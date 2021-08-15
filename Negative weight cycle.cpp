107d807cfedd20c86799016c1701501a6dc4c529
Shayak <44469461+shr0nin@users.noreply.github.com>
2021-08-15 19:02:57 +0530

    Update Negative weight cycle.cpp
    https://practice.geeksforgeeks.org/problems/negative-weight-cycle3504/1

--- Negative weight cycle.cpp
+++ Negative weight cycle.cpp
@@ -1,39 +1,27 @@
-#include <bits/stdc++.h>
-using namespace std;
-
-#define INF 2e9
- 
-int main() {
-    int t;
-    scanf("%d", &t);
-    while(t--) {
-        int v,e;
-        scanf("%d%d", &v,&e);
-        vector<pair<pair<int,int>,int>> edges;
-        for(int i=0; i<e; ++i) {
-            int x,y,z;
-            scanf("%d%d%d", &x,&y,&z);
-            edges.push_back({{x,y},z});
-        }
-        vector<int> dist(v, INF);
-        dist[0]=0;
-        for(int i=1; i<v; ++i) {
-            for(auto j: edges) {
-                dist[j.first.second]=min(dist[j.first.second], dist[j.first.first]+j.second);
-            }
-        }
-        bool flag=true;
-        for(auto j: edges) {
-            if(dist[j.first.first]+j.second < dist[j.first.second]) {
-                puts("1");
-                flag=false;
-                break;
-            }
-        }
-        if(flag) {
-            puts("0");
-        }
-    }
- 
-    return 0;
-}
+class Solution {
+    const int INF=2e9+5;
+public:
+	int isNegativeWeightCycle(int n, vector<vector<int>>edges){
+	    vector<int> dist(n, INF);
+	    dist[0]=0;
+	    for(int i=1; i<n; ++i) {
+	        for(auto e: edges) {
+	            if(dist[e[0]]!=INF) {
+	                dist[e[1]]=min(dist[e[1]], dist[e[0]]+e[2]);
+	            }
+	        }
+	    }
+	    for(int i=1; i<n; ++i) {
+	        for(auto e: edges) {
+    	       // if(dist[e[0]]==INF) {
+    	       //     return 1;
+    	       // }
+    	        if(dist[e[0]]+e[2]<dist[e[1]]) {
+    	            dist[e[1]]=INF;
+    	            return 1;
+    	        }
+	        }
+	    }
+	    return 0;
+	}
+};
