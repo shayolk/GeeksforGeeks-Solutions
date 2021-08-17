fb5cd419a3b7ae9a1bfdcbc1d54205c4c2a01d7c
Shayak <44469461+shr0nin@users.noreply.github.com>
2021-08-18 00:58:15 +0530

    Update Subset Sum Problem.cpp
    https://practice.geeksforgeeks.org/problems/subset-sum-problem-1611555638/1/?category[]=Dynamic%20Programming&category[]=Dynamic%20Programming&page=4&query=category[]Dynamic%20Programmingpage4category[]Dynamic%20Programming

--- Subset Sum Problem.cpp
+++ Subset Sum Problem.cpp
@@ -1,58 +1,25 @@
-#include <bits/stdc++.h>
-using namespace std;
+//User function template for C++
 
-// bool check(vector<int> a, int sum, int i, int now) {
-//     if(i<0 || now>sum) {
-//         return false;
-//     }
-//     if(now==sum) {
-//         return true;
-//     }
-//     return check(a,sum,i-1,now) || check(a,sum,i-1,now+a[i]);
-// }
-
-int main() {
-    int t;
-    scanf("%d", &t);
-    while(t--) {
-        int n, sum=0;
-        scanf("%d", &n);
-        vector<int> a(n);
-        for(int& i: a) {
-            scanf("%d", &i);
-            sum+=i;
+class Solution{   
+public:
+    bool isSubsetSum(int n, int arr[], int sum){
+        int tot=0;
+        for(int i=0; i<n; ++i) tot+=arr[i];
+        if(tot<sum) return false;
+        int ans=false;
+        vector<vector<bool>> dp(n+1, vector<bool> (tot+1));
+        for(int i=0; i<=n; ++i) {
+            dp[i][0]=true;
         }
-       // if(sum&1 || !check(a, sum/2, n-1, 0)) {
-       //     printf("NO\n");
-       //     continue;
-       // }
-       // printf("YES\n");
-        if(sum&1) {
-            printf("NO\n");
-            continue;
-        }
-        sum/=2;
-        vector<bool> dp(sum+1);
-        dp[0]=true;
-        for(int i: a) {
-            vector<int> ind;
-            for(int j=0; j+i<=sum; ++j) {
-                if(dp[j]) {
-                   // dp[j+i]=true;
-                    ind.emplace_back(j+i);
+        for(int i=1; i<=n; ++i) {
+            for(int j=1; j<=tot; ++j) {
+                dp[i][j]=dp[i-1][j];
+                if(j-arr[i-1]>=0) {
+                    dp[i][j]=dp[i][j] || dp[i-1][j-arr[i-1]];
                 }
-            }
-            for(int j: ind) {
-                dp[j]=true;
+                if(j==sum) ans=ans || dp[i][j];
             }
         }
-        if(dp[sum]) {
-            printf("YES\n");
-        }
-        else {
-            printf("NO\n");
-        }
+        return ans;
     }
- 
-    return 0;
-}
+};
