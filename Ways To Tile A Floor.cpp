5b95f4d10703654b854c40bd900120f717662a6d
Shayak <44469461+shr0nin@users.noreply.github.com>
2021-08-21 19:44:15 +0530

    Update Ways To Tile A Floor.cpp
    https://practice.geeksforgeeks.org/problems/ways-to-tile-a-floor5836/1#

--- Ways To Tile A Floor.cpp
+++ Ways To Tile A Floor.cpp
@@ -1,27 +1,13 @@
-#include <bits/stdc++.h>
-using namespace std;
-
-const int nax=71;
-vector<long> dp(nax);
-
-void init() {
-    dp[1]=1;
-    dp[2]=2;
-    for(int i=3; i<nax; ++i) {
-        dp[i]+=dp[i-1];
-        dp[i]+=dp[i-2];
+class Solution {
+    const int M=1e9+7;
+    
+  public:
+    long long numberOfWays(long long N) {
+        long long dp[N+1];
+        dp[0]=dp[1]=1;
+        for(int i=2; i<=N; ++i) {
+            dp[i]=(dp[i-1]+dp[i-2])%M;
+        }
+        return dp[N];
     }
-}
- 
-int main() {
-    init();
-    int t;
-    scanf("%d", &t);
-    while(t--) {
-        int w;
-        scanf("%d", &w);
-        printf("%ld\n", dp[w]);
-    }
- 
-    return 0;
-}
+};
