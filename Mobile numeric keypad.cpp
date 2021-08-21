9a9ee40671b1ffb4a807cff7deb5a20360ec2344
Shayak <44469461+shr0nin@users.noreply.github.com>
2021-08-21 12:56:44 +0530

    Update Mobile numeric keypad.cpp
    https://practice.geeksforgeeks.org/problems/mobile-numeric-keypad5456/1#

--- Mobile numeric keypad.cpp
+++ Mobile numeric keypad.cpp
@@ -1,100 +1,46 @@
-#include <bits/stdc++.h>
-using namespace std;
-
-vector<vector<int>> neb;
-
-void init() {
-    vector<int> v;
-    // 0
-    v.emplace_back(8);
-    neb.emplace_back(v);
-    // 1
-    v.clear();
-    v.emplace_back(2);
-    v.emplace_back(4);
-    neb.emplace_back(v);
-    // 2
-    v.clear();
-    v.emplace_back(1);
-    v.emplace_back(3);
-    v.emplace_back(5);
-    neb.emplace_back(v);
-    // 3
-    v.clear();
-    v.emplace_back(2);
-    v.emplace_back(6);
-    neb.emplace_back(v);
-    // 4
-    v.clear();
-    v.emplace_back(1);
-    v.emplace_back(5);
-    v.emplace_back(7);
-    neb.emplace_back(v);
-    // 5
-    v.clear();
-    v.emplace_back(2);
-    v.emplace_back(4);
-    v.emplace_back(6);
-    v.emplace_back(8);
-    neb.emplace_back(v);
-    // 6
-    v.clear();
-    v.emplace_back(3);
-    v.emplace_back(5);
-    v.emplace_back(9);
-    neb.emplace_back(v);
-    // 7
-    v.clear();
-    v.emplace_back(4);
-    v.emplace_back(8);
-    neb.emplace_back(v);
-    // 8
-    v.clear();
-    v.emplace_back(0);
-    v.emplace_back(5);
-    v.emplace_back(7);
-    v.emplace_back(9);
-    neb.emplace_back(v);
-    // 9
-    v.clear();
-    v.emplace_back(6);
-    v.emplace_back(8);
-    neb.emplace_back(v);
-}
-
-const int nax=32;
-vector<vector<long long>> dp(nax, vector<long long> (10));
-vector<long long> pref(nax);
-
-void build() {
-    for(int j=0; j<10; ++j) {
-        dp[1][j]=1;
+class Solution{
+    vector<vector<char>> key={
+        {'1', '2', '3'},
+        {'4', '5', '6'},
+        {'7', '8', '9'},
+        {'*', '0', '#'}
+    };
+    
+    const int dr[5]={0, 0, 0, -1, 1};
+    const int dc[5]={0, -1, 1, 0, 0};
+    long long dp[10][30];
+    
+    bool ok(int i, int j) {
+        return i>=0 && i<4 && j>=0 && j<3 && key[i][j]!='*' && key[i][j]!='#';
     }
-    for(int i=2; i<nax; ++i) {
-        for(int j=0; j<10; ++j) {
-            dp[i][j]=dp[i-1][j];
-            for(int his: neb[j]) {
-                dp[i][j]+=dp[i-1][his];
-            }
-        }
-    }
-    for(int i=0; i<nax; ++i) {
-        for(int j=0; j<10; ++j) {
-            pref[i]+=dp[i][j];
-        }
-    }
-}
- 
-int main() {
-    init();
-    build();
-	int t;
-	scanf("%d", &t);
-	while(t--) {
-	    int n;
-	    scanf("%d", &n);
-	    printf("%lld\n", pref[n]);
+
+	public:
+	long long getCount(int N)
+	{
+	    for(int i=0; i<10; ++i) {
+	        dp[i][1]=1;
+	    }
+	    for(int i=0; i<4; ++i) {
+	        for(int j=0; j<3; ++j) {
+	            if(!ok(i, j)) continue;
+	            int now=key[i][j]-'0';
+	            for(int k=2; k<=N; ++k) {
+	                dp[now][k]=0;
+	                for(int d=0; d<5; ++d) {
+	                    int r=i+dr[d], c=j+dc[d];
+	                    if(ok(r, c)) {
+	                        dp[now][k]+=dp[key[r][c]-'0'][k-1];
+	                    }
+	                }
+	            }
+	        }
+	    }
+	    long long ans=0;
+	    for(int i=0; i<10; ++i) {
+	        ans+=dp[i][N];
+	    }
+	    return ans;
 	}
- 
-    return 0;
-}
+
+
+};
