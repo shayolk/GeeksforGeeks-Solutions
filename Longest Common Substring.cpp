7e9c370c913e4aa5709a807ae4efdacb6599d693
Shayak <44469461+shr0nin@users.noreply.github.com>
2021-08-29 11:57:28 +0530

    Update Longest Common Substring.cpp
    https://practice.geeksforgeeks.org/problems/longest-common-substring1452/1

--- Longest Common Substring.cpp
+++ Longest Common Substring.cpp
@@ -1,26 +1,18 @@
-#include <bits/stdc++.h>
-using namespace std;
-
-int main() {
-    int t;
-    scanf("%d", &t);
-    while(t--) {
-        int n,m;
-        scanf("%d%d", &n,&m);
-        char a[n+1], b[m+1];
-        scanf("%s%s", &a,&b);
-        vector<vector<int>> dp(n+1, vector<int>(m+1));
-        int sz=0;
+class Solution{
+    public:
+    
+    int longestCommonSubstr (string S1, string S2, int n, int m)
+    {
+        vector<vector<int>> dp(n+1, vector<int> (m+1));
+        int len=0;
         for(int i=1; i<=n; ++i) {
             for(int j=1; j<=m; ++j) {
-                if(a[i-1]==b[j-1]) {
+                if(S1[i-1]==S2[j-1]) {
                     dp[i][j]=1+dp[i-1][j-1];
-                    sz=max(sz, dp[i][j]);
+                    len=max(len, dp[i][j]);
                 }
             }
         }
-        printf("%d\n", sz);
+        return len;
     }
-    
-    return 0;
-}
+};
