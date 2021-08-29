14be51b7913510578f2f42780ca65d31fc23878d
Shayak <44469461+shr0nin@users.noreply.github.com>
2021-08-29 16:40:24 +0530

    Update Find median in a stream.cpp
    https://practice.geeksforgeeks.org/problems/find-median-in-a-stream-1587115620/1

--- Find median in a stream.cpp
+++ Find median in a stream.cpp
@@ -1,27 +1,33 @@
-#include <bits/stdc++.h>
-using namespace std;
-
-int main() {
-    int n;
-    scanf("%d", &n);
+class Solution
+{
     priority_queue<int> mx;
-    priority_queue<int,vector<int>,greater<int>> mn;
-    for(int i=0; i<n; ++i) {
-        int x;
-        scanf("%d", &x);
-        if((int)mx.size()>(int)mn.size()) {
-            mx.push(x);
+    priority_queue<int, vector<int>, greater<int>> mn;
+    public:
+    //Function to insert heap.
+    void insertHeap(int &x)
+    {
+        if(mx.size()>mn.size()) mx.push(x);
+        else mn.push(x);
+        balanceHeaps();
+    }
+    
+    //Function to balance heaps.
+    void balanceHeaps()
+    {
+        if(mx.size()>mn.size()) {
             mn.push(mx.top());
             mx.pop();
-            printf("%d\n", (mx.top()+mn.top())/2);
         }
         else {
-            mn.push(x);
             mx.push(mn.top());
             mn.pop();
-            printf("%d\n", mx.top());
         }
     }
     
-    return 0;
-}
+    //Function to return Median.
+    double getMedian()
+    {
+        if(mx.size()>mn.size()) return mx.top();
+        else return (mn.top()+mx.top())/2.0;
+    }
+};
