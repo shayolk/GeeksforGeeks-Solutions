adf25eb80b710f188718f023360ebb9fed60b01d
Shayak <44469461+shr0nin@users.noreply.github.com>
2021-08-31 20:41:09 +0530

    Create Minimize the Heights II.cpp
    https://practice.geeksforgeeks.org/problems/minimize-the-heights3351/1#

---
+++ Minimize the Heights II.cpp
@@ -0,0 +1,16 @@
+// User function template for C++
+
+class Solution {
+  public:
+    int getMinDiff(int arr[], int n, int k) {
+        sort(arr, arr+n);
+        int diff=arr[n-1]-arr[0];
+        for(int i=1; i<n; ++i) {
+            if(arr[i]<k) continue;
+            int mn=min(arr[i]-k, arr[0]+k);
+            int mx=max(arr[i-1]+k, arr[n-1]-k);
+            diff=min(diff, mx-mn);
+        }
+        return diff;
+    }
+};
