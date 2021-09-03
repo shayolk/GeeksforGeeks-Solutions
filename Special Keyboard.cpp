1b48c1fab3361708a896d0233d3f7f6540e2dbd6
Shayak <44469461+shr0nin@users.noreply.github.com>
2021-09-03 22:26:04 +0530

    Update Special Keyboard.cpp
    https://practice.geeksforgeeks.org/problems/special-keyboard3018/1#

--- Special Keyboard.cpp
+++ Special Keyboard.cpp
@@ -1,27 +1,16 @@
-#include <bits/stdc++.h>
-using namespace std;
+// User function Template for C++
 
-const int nax=80;
-vector<int> dp(nax);
-
-void init() {
-    for(int i=1; i<=6; ++i) {
-        dp[i]=i;
+class Solution{
+public:
+    long long int optimalKeys(int N){
+        long long dp[N+1];
+        for(int i=1; i<=6; ++i) {
+            dp[i]=i;
+            if(i==N) return dp[N];
+        }
+        for(int i=7; i<=N; ++i) {
+            dp[i]=max({1+dp[i-1], 2*dp[i-3], 3*dp[i-4], 4*dp[i-5]});
+        }
+        return dp[N];
     }
-    for(int i=7; i<nax; ++i) {
-        dp[i]=max({dp[i-1]+1, 2*dp[i-3], 3*dp[i-4], 4*dp[i-5]});
-    }
-}
-
-int main() {
-    init();
-    int t;
-    scanf("%d", &t);
-    while(t--) {
-        int n;
-        scanf("%d", &n);
-        printf("%d\n", dp[n]);
-    }
-    
-    return 0;
-}
+};
