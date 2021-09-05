f5057da087d96e791f411c22cfa212e901eb9100
Shayak <44469461+shr0nin@users.noreply.github.com>
2021-09-05 13:00:28 +0530

    Update Rotate Array.cpp
    https://practice.geeksforgeeks.org/problems/rotate-array-by-n-elements-1587115621/1

--- Rotate Array.cpp
+++ Rotate Array.cpp
@@ -1,21 +1,10 @@
-#include <bits/stdc++.h>
-using namespace std;
-
-int main() {
-    int t;
-    scanf("%d", &t);
-    while(t--) {
-        int n,d;
-        scanf("%d%d", &n,&d);
-        vector<int> a(n);
-        for(int i=0; i<n; ++i) {
-            scanf("%d", &a[(i-d+n)%n]);
-        }
-        for(int i=0; i<n; ++i) {
-            printf("%d ", a[i]);
-        }
-        printf("\n");
-    }
+class Solution{
+    public:
     
-    return 0;
-}
+    //Function to rotate an array by d elements in counter-clockwise direction. 
+    void rotateArr(int arr[], int d, int n){
+        reverse(arr, arr+d);
+        reverse(arr+d, arr+n);
+        reverse(arr, arr+n);
+    }
+};
