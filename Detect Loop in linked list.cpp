60c885466c1bc1ecd814c80569d9c4bf55df3ae1
Shayak <44469461+shr0nin@users.noreply.github.com>
2021-09-08 10:52:51 +0530

    Update Detect Loop in linked list.cpp
    https://practice.geeksforgeeks.org/problems/detect-loop-in-linked-list/1

--- Detect Loop in linked list.cpp
+++ Detect Loop in linked list.cpp
@@ -1,3 +1,5 @@
+//User function template for C++
+
 /*
 
 struct Node
@@ -10,13 +12,18 @@ struct Node
     }
 
 */
-
-bool detectLoop(Node* head) {
-    Node *slow=head, *fast=head;
-    while(fast->next && fast->next->next) {
-        slow=slow->next;
-        fast=fast->next->next;
-        if(slow==fast) return true;
+class Solution
+{
+    public:
+    //Function to check if the linked list has a loop.
+    bool detectLoop(Node* head)
+    {
+        Node *slow=head, *fast=head;
+        while(fast && fast->next) {
+            slow=slow->next;
+            fast=fast->next->next;
+            if(slow==fast) return true;
+        }
+        return false;
     }
-    return false;
-}
+};
