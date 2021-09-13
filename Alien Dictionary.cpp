75c347bba202641ecca4be63a7e3a886430be9aa
Shayak <44469461+shr0nin@users.noreply.github.com>
2021-09-13 20:58:46 +0530

    Update Alien Dictionary.cpp
    https://practice.geeksforgeeks.org/problems/alien-dictionary/1

--- Alien Dictionary.cpp
+++ Alien Dictionary.cpp
@@ -3,33 +3,38 @@ dict : array of strings denoting the words in alien langauge
 N : Size of the dictionary
 K : Number of characters
 */
-void dfs(int now, vector<set<int>>& adj, vector<bool>& vis, string& topo) {
-    vis[now]=true;
-    for(auto neb=adj[now].begin(); neb!=adj[now].end(); ++neb) {
-        if(!vis[*neb]) {
-            dfs(*neb,adj,vis,topo);
-        }
-    }
-    topo+='a'+now;
-}
 
-string findOrder(string dict[], int N, int K) {
-    vector<set<int>> adj(K);
-    for(int i=1; i<N; ++i) {
-        for(int j=0; j<min((int)dict[i-1].size(), (int)dict[i].size()); ++j) {
-            if(dict[i-1][j]!=dict[i][j]) {
-                adj[dict[i-1][j]-'a'].insert(dict[i][j]-'a');
-                break;
+// User function Template for C++
+
+class Solution{
+    vector<set<int>> adj;
+    vector<bool> vis;
+    string topo;
+    
+    void dfs(int now) {
+        vis[now]=true;
+        for(int neb: adj[now]) {
+            if(!vis[neb]) dfs(neb);
+        }
+        topo+=('a'+now);
+    }
+    
+    public:
+    string findOrder(string dict[], int N, int K) {
+        adj.resize(K);
+        vis.resize(K);
+        for(int i=1; i<N; ++i) {
+            for(int j=0; j<min(dict[i-1].size(), dict[i].size()); ++j) {
+                if(dict[i-1][j]!=dict[i][j]) {
+                    adj[dict[i-1][j]-'a'].insert(dict[i][j]-'a');
+                    break;
+                }
             }
         }
-    }
-    vector<bool> vis(K);
-    string topo;
-    for(int i=0; i<K; ++i) {
-        if(!vis[i]) {
-            dfs(i,adj,vis,topo);
+        for(int i=0; i<K; ++i) {
+            if(!vis[i]) dfs(i);
         }
+        reverse(topo.begin(), topo.end());
+        return topo;
     }
-    reverse(topo.begin(), topo.end());
-    return topo;
-}
+};
