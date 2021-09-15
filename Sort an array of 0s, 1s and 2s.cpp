7b7fccf686283c3cafb95b16f872529086d2037f
Shayak <44469461+shr0nin@users.noreply.github.com>
2021-09-15 23:37:33 +0530

    Update Sort an array of 0s, 1s and 2s.cpp
    https://practice.geeksforgeeks.org/problems/sort-an-array-of-0s-1s-and-2s4231/1

--- Sort an array of 0s, 1s and 2s.cpp
+++ Sort an array of 0s, 1s and 2s.cpp
@@ -1,24 +1,18 @@
-#include <bits/stdc++.h>
-using namespace std;
-
-int main() {
-    int t;
-    scanf("%d", &t);
-    while(t--) {
-        int n,x;
-        scanf("%d", &n);
-        vector<int> a(3);
-        while(n--) {
-            scanf("%d", &x);
-            ++a[x];
+class Solution
+{
+    public:
+    void sort012(int a[], int n)
+    {
+        vector<int> cnt(3);
+        for(int i=0; i<n; ++i) {
+            ++cnt[a[i]];
         }
+        int j=0;
         for(int i=0; i<3; ++i) {
-            for(int j=0; j<a[i]; ++j) {
-                printf("%d ", i);
+            while(cnt[i]--) {
+                a[j++]=i;
             }
         }
-        printf("\n");
     }
     
-    return 0;
-}
+};
