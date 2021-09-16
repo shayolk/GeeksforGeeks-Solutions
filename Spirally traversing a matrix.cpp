19dba61d69ea4e994876f56e463369652479d7c5
Shayak <44469461+shr0nin@users.noreply.github.com>
2021-09-16 20:52:09 +0530

    Update Spirally traversing a matrix.cpp
    https://practice.geeksforgeeks.org/problems/spirally-traversing-a-matrix-1587115621/1

--- Spirally traversing a matrix.cpp
+++ Spirally traversing a matrix.cpp
@@ -1,48 +1,38 @@
-#include <bits/stdc++.h>
-using namespace std;
-
-int main() {
-    int t;
-    scanf("%d", &t);
-    while(t--) {
-        int n,m;
-        scanf("%d%d", &n,&m);
-        vector<vector<int>> a(n, vector<int>(m));
-        for(int i=0; i<n; ++i) {
-            for(int j=0; j<m; ++j) {
-                scanf("%d", &a[i][j]);
-            }
-        }
+class Solution
+{   
+    public: 
+    //Function to return a list of integers denoting spiral traversal of matrix.
+    vector<int> spirallyTraverse(vector<vector<int> > a, int n, int m) 
+    {
+        vector<int> ans;
         int i=0, j=-1, dir=0, horl=0, horr=m-1, verl=0, verr=n-1;
         while(horl<=horr && verl<=verr) {
             if(dir==0) {
                 ++j;
-                while(j<=horr) printf("%d ", a[i][j++]);
+                while(j<=horr) ans.push_back(a[i][j++]);
                 --j;
                 ++verl;
             }
             else if(dir==1) {
                 ++i;
-                while(i<=verr) printf("%d ", a[i++][j]);
+                while(i<=verr) ans.push_back(a[i++][j]);
                 --i;
                 --horr;
             }
             else if(dir==2) {
                 --j;
-                while(j>=horl) printf("%d ", a[i][j--]);
+                while(j>=horl) ans.push_back(a[i][j--]);
                 ++j;
                 --verr;
             }
             else {
                 --i;
-                while(i>=verl) printf("%d ", a[i--][j]);
+                while(i>=verl) ans.push_back(a[i--][j]);
                 ++i;
                 ++horl;
             }
             dir=(dir+1)%4;
         }
-        printf("\n");
+        return ans;
     }
-    
-    return 0;
-}
+};
