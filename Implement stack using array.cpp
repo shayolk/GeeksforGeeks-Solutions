a0e91be8ba51a6b54f74826ab864bc9efb128a83
Shayak <44469461+shr0nin@users.noreply.github.com>
2021-12-26 17:08:58 +0530

    Create Implement stack using array.cpp
    https://practice.geeksforgeeks.org/problems/implement-stack-using-array/1/?company[]=Goldman%20Sachs&company[]=Goldman%20Sachs&page=1&query=company[]Goldman%20Sachspage1company[]Goldman%20Sachs

---
+++ Implement stack using array.cpp
@@ -0,0 +1,13 @@
+//Function to push an integer into the stack.
+void MyStack :: push(int x)
+{
+    if(top==999) return;
+    arr[++top]=x;
+}
+
+//Function to remove an item from top of the stack.
+int MyStack :: pop()
+{
+    if(top==-1) return -1;
+    return arr[top--];
+}
