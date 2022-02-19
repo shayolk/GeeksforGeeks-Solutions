c5b30907b31399ee3d3a9702e121073acc5cf8ab
Shayak <44469461+shr0nin@users.noreply.github.com>
2022-02-19 20:32:19 +0530

    Update Rat in a Maze Problem - I.cpp
    https://practice.geeksforgeeks.org/problems/rat-in-a-maze-problem/1

--- Rat in a Maze Problem - I.cpp
+++ Rat in a Maze Problem - I.cpp
@@ -1,39 +1,42 @@
 // User function template for C++
 
-// m is the given matrix and n is the order of matrix
-// MAX is the upper limit of N ie 5
-
-const vector<int> dr={0, 0, 1, -1};
-const vector<int> dc={1, -1, 0, 0};
-
-bool safe(int i, int j, int n) {
-    return i>=0 && i<n && j>=0 && j<n;
-}
-
-void back(int i, int j, int n, int m[MAX][MAX], string s, vector<string>& ans) {
-    if(i==n-1 && j==n-1) {
-        ans.push_back(s);
-        return;
-    }
-    for(int x=0; x<4; ++x) {
-        int r=i+dr[x], c=j+dc[x];
-        if(safe(r,c,n) && m[r][c]) {
-            if(x==0) s+='R';
-            else if(x==1) s+='L';
-            else if(x==2) s+='D';
-            else s+='U';
-            m[i][j]=0;
-            back(r,c,n,m,s,ans);
-            s.pop_back();
-            m[i][j]=1;
-        }
-    }
-}
-
-vector<string> printPath(int m[MAX][MAX], int n) {
+class Solution{
+    int dr[4]={0, 0, 1, -1};
+    int dc[4]={1, -1, 0, 0};
+    char dir[4]={'R', 'L', 'D', 'U'};
+    int n;
+    vector<vector<bool>> vis;
     vector<string> ans;
-    string s;
-    if(m[0][0]) back(0,0,n,m,s,ans);
-    sort(ans.begin(), ans.end());
-    return ans;
-}
+    
+    bool safe(int r, int c) {
+        return r>=0 && r<n && c>=0 && c<n;
+    }
+    
+    void find(int i, int j, string& s, vector<vector<int>>& m) {
+        if(i==n-1 && j==n-1) {
+            ans.push_back(s);
+            return;
+        }
+        m[i][j]=0;
+        for(int d=0; d<4; ++d) {
+            int r=i+dr[d], c=j+dc[d];
+            if(!safe(r, c) || vis[r][c] || !m[r][c]) continue;
+            vis[r][c]=true;
+            s+=dir[d];
+            find(r, c, s, m);
+            s.pop_back();
+            vis[r][c]=false;
+        }
+        m[i][j]=1;
+    }
+    
+    public:
+    vector<string> findPath(vector<vector<int>> &m, int n) {
+        if(!m[0][0]) return ans;
+        this->n=n;
+        vis.resize(n, vector<bool> (n));
+        string s;
+        find(0, 0, s, m);
+        return ans;
+    }
+};
