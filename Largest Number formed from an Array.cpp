191d41fbf2fa15672f005ef4ca38c7c9d84f6381
Shayak <44469461+shr0nin@users.noreply.github.com>
2022-03-05 17:14:25 +0530

    Update Largest Number formed from an Array.cpp
    https://practice.geeksforgeeks.org/problems/largest-number-formed-from-an-array1117/1/#

--- Largest Number formed from an Array.cpp
+++ Largest Number formed from an Array.cpp
@@ -1,47 +1,20 @@
-#include <bits/stdc++.h>
-using namespace std;
-
-bool comp(const string a, const string b) {
-    // return stoi(a+b) > stoi(b+a);
-    int n=(int)a.size(), m=(int)b.size();
-    for(int i=0; i<min(n,m); ++i) {
-        if(a[i]!=b[i]) return a[i]>b[i];
-    }
-    if(n>m) {
-        for(int i=m; i<n; ++i) {
-            if(a[i]!=a[i-m]) return a[i]>a[i-m];
-        }
-        for(int i=0; i<m && m-1+i<n; ++i) {
-        	if(a[m-1+i]!=b[i]) return a[m-1+i]<b[i];
-        }
-    }
-    if(m>n) {
-	    for(int i=n; i<m; ++i) {
-	        if(b[i]!=b[i-n]) return b[i]<b[i-n];
-	    }
-	    for(int i=0; i<n && n-1+i<m; ++i) {
-        	if(b[n-1+i]!=a[i]) return b[n-1+i]>a[i];
-        }
-	}
-	return a>b;
-}
-
-int main() {
-    int t;
-    scanf("%d", &t);
-    while(t--) {
-        int n;
-        scanf("%d", &n);
-        vector<string> a(n);
-        for(string& i:a) {
-            cin>>i;
-        }
-        sort(a.begin(), a.end(), comp);
-        for(string i:a) {
-            cout<<i;
-        }
-        printf("\n");
+//User function template for C++
+class Solution{
+    static bool comp(const string& a, const string& b) {
+        string ab=a+b, ba=b+a;
+        return ab>ba;
     }
     
-    return 0;
-}
+public:
+	// The main function that returns the arrangement with the largest value as
+	// string.
+	// The function accepts a vector of strings
+	string printLargest(vector<string> &arr) {
+	    sort(arr.begin(), arr.end(), comp);
+	    string ans;
+	    for(string s: arr) {
+	        ans+=s;
+	    }
+	    return ans;
+	}
+};
