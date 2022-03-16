1617ec1b9499973b50841091f7f2676001fb529a
Shayak <44469461+shr0nin@users.noreply.github.com>
2022-03-16 18:11:01 +0530

    Update Left View of Binary Tree.cpp
    https://practice.geeksforgeeks.org/problems/left-view-of-binary-tree/1

--- Left View of Binary Tree.cpp
+++ Left View of Binary Tree.cpp
@@ -17,19 +17,19 @@ struct Node
 vector<int> leftView(Node *root)
 {
     map<int,int> view;
-        queue<pair<Node*, int>> q;
-        q.push({root, 0});
-        while(!q.empty()) {
-            pair<Node*, int> p=q.front();
-            q.pop();
-            if(!p.first) continue;
-            if(!view.count(p.second)) view[p.second]=p.first->data;
-            q.push({p.first->left, p.second+1});
-            q.push({p.first->right, p.second+1});
-        }
-        vector<int> ans;
-        for(auto v: view) {
-            ans.push_back(v.second);
-        }
-        return ans;
+    queue<pair<Node*, int>> q;
+    q.push({root, 0});
+    while(!q.empty()) {
+        pair<Node*, int> p=q.front();
+        q.pop();
+        if(!p.first) continue;
+        if(!view.count(p.second)) view[p.second]=p.first->data;
+        q.push({p.first->left, p.second+1});
+        q.push({p.first->right, p.second+1});
+    }
+    vector<int> ans;
+    for(auto v: view) {
+        ans.push_back(v.second);
+    }
+    return ans;
 }
