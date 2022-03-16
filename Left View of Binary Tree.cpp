469508ad3aeb37ca5bb869b304b6c6705c2bf14e
Shayak <44469461+shr0nin@users.noreply.github.com>
2022-03-16 16:53:08 +0530

    Update Left View of Binary Tree.cpp
    https://practice.geeksforgeeks.org/problems/left-view-of-binary-tree/1#

--- Left View of Binary Tree.cpp
+++ Left View of Binary Tree.cpp
@@ -13,17 +13,22 @@ struct Node
 };
  */
 
-void dfs(Node* root, int level, vector<bool>& vis) {
-    if(root==NULL) return;
-    if(!vis[level]) {
-        printf("%d ", root->data);
-        vis[level]=true;
+//Function to return a list containing elements of left view of the binary tree.
+void visit(Node* root, int depth, map<int,bool>& done, vector<int>& ans) {
+    if(!root) return;
+    if(!done.count(depth)) {
+        done[depth]=true;
+        ans.push_back(root->data);
     }
-    dfs(root->left,level+1,vis);
-    dfs(root->right,level+1,vis);
+    visit(root->left, depth+1, done, ans);
+    visit(root->right, depth+1, done, ans);
+    
 }
 
-void leftView(Node *root) {
-    vector<bool> vis(105, false);
-    dfs(root,1,vis);
+vector<int> leftView(Node *root)
+{
+    map<int,bool> done;
+    vector<int> ans;
+    visit(root, 0, done, ans);
+    return ans;
 }
