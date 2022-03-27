f0f2275fde9ed577954e84d149e12247ccefb1e1
Shayak <44469461+shr0nin@users.noreply.github.com>
2022-03-27 19:51:26 +0530

    Update BFS of graph.cpp
    https://practice.geeksforgeeks.org/problems/bfs-traversal-of-graph/1

--- BFS of graph.cpp
+++ BFS of graph.cpp
@@ -1,23 +1,23 @@
-/* Function to do BFS of graph
-*  g[]: adj list of the graph
-*  N : number of vertices
-*/
-vector <int> bfs(vector<int> g[], int N) {
-    vector<int> ret;
-    queue<int> q;
-    q.push(0);
-    vector<bool> vis(N);
-    vis[0]=true;
-    while(!q.empty()) {
-        int now=q.front();
-        q.pop();
-        ret.push_back(now);
-        for(int neb: g[now]) {
-            if(!vis[neb]) {
-                q.push(neb);
-                vis[neb]=true;
+class Solution {
+  public:
+    // Function to return Breadth First Traversal of given graph.
+    vector<int> bfsOfGraph(int V, vector<int> adj[]) {
+        vector<int> ans;
+        queue<int> q;
+        q.push(0);
+        vector<bool> vis(V);
+        vis[0]=true;
+        while(!q.empty()) {
+            int now=q.front();
+            q.pop();
+            ans.push_back(now);
+            for(int neb: adj[now]) {
+                if(!vis[neb]) {
+                    q.push(neb);
+                    vis[neb]=true;
+                }
             }
         }
+        return ans;
     }
-    return ret;
-}
+};
