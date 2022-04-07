5093a453aa3019d6fc2e7427b0fa6ad932610dea
Shayak <44469461+shr0nin@users.noreply.github.com>
2022-04-07 14:57:39 +0530

    Update Count pairs with given sum.cpp
    https://practice.geeksforgeeks.org/problems/count-pairs-with-given-sum5022/1

--- Count pairs with given sum.cpp
+++ Count pairs with given sum.cpp
@@ -1,25 +1,14 @@
-#include <bits/stdc++.h>
-using namespace std;
+//User function template for C++
 
-int main() {
-    int t;
-    scanf("%d", &t);
-    while(t--) {
-        int n,k;
-        scanf("%d%d", &n,&k);
-        vector<int> a(n);
-        for(int& i: a) {
-            scanf("%d", &i);
-        }
-        map<int,int> m;
-        ++m[a[n-1]];
+class Solution{   
+public:
+    int getPairsCount(int arr[], int n, int k) {
+        map<int,int> cnt;
         int ans=0;
-        for(int i=n-2; i>=0; --i) {
-            ans+=m[k-a[i]];
-            ++m[a[i]];
+        for(int i=0; i<n; ++i) {
+            if(cnt.count(k-arr[i])) ans+=cnt[k-arr[i]];
+            ++cnt[arr[i]];
         }
-        printf("%d\n", ans);
+        return ans;
     }
-    
-    return 0;
-}
+};
