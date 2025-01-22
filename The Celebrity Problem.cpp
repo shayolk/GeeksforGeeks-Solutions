f33ef386a7e511dcdca05d308e01b6c02509c95d
Shayak <44469461+shayolk@users.noreply.github.com>
2025-01-22 20:50:03 +0000

    https://www.geeksforgeeks.org/problems/the-celebrity-problem/1

--- The Celebrity Problem.cpp
+++ The Celebrity Problem.cpp
@@ -1,25 +1,25 @@
-//User function template for C++
-
-class Solution 
-{
-    public:
-    //Function to find if there is a celebrity in the party or not.
-    int celebrity(vector<vector<int>>& M, int n) 
-    {
-        vector<int> a(n);
-        iota(a.begin(), a.end(), 0);
-        while(a.size()>1) {
-            int A=a.back();
-            a.pop_back();
-            int B=a.back();
-            a.pop_back();
-            if(M[A][B]) a.push_back(B);
-            else a.push_back(A);
+class Solution {
+  public:
+    // Function to find if there is a celebrity in the party or not.
+    int celebrity(vector<vector<int> >& mat) {
+        int n = mat.size();
+        int l = 0, r = n - 1;
+        while(l < r) {
+            if(mat[l][r]) {
+                ++l;
+            }
+            else {
+                --r;
+            }
         }
-        assert(a.size()==1);
         for(int i=0; i<n; ++i) {
-            if(i!=a[0] && (!M[i][a[0]] || M[a[0]][i])) return -1;
+            if(mat[l][i]) {
+                return -1;
+            }
+            if(i != l && mat[i][l] == 0) {
+                return -1;
+            }
         }
-        return a[0];
+        return l;
     }
 };
