3e85d67e1e6bb57d5e4a4495d411973b813dca4b
Shayak <44469461+shayolk@users.noreply.github.com>
2025-01-26 19:51:23 +0000

    https://www.geeksforgeeks.org/problems/left-view-of-binary-tree/1

--- Left View of Binary Tree.cpp
+++ Left View of Binary Tree.cpp
@@ -5,7 +5,7 @@ struct Node
     int data;
     struct Node* left;
     struct Node* right;
-    
+
     Node(int x){
         data = x;
         left = right = NULL;
@@ -13,23 +13,35 @@ struct Node
 };
  */
 
-//Function to return a list containing elements of left view of the binary tree.
-vector<int> leftView(Node *root)
-{
-    map<int,int> view;
-    queue<pair<Node*, int>> q;
-    q.push({root, 0});
-    while(!q.empty()) {
-        pair<Node*, int> p=q.front();
-        q.pop();
-        if(!p.first) continue;
-        if(!view.count(p.second)) view[p.second]=p.first->data;
-        q.push({p.first->left, p.second+1});
-        q.push({p.first->right, p.second+1});
+class Solution {
+  public:
+    vector<int> leftView(Node *root) {
+        vector<int> view;
+        if(!root) {
+            return view;
+        }
+        queue<Node*> q;
+        q.push(root);
+        q.push(NULL);
+        while(!q.empty()) {
+            Node* front = q.front();
+            q.pop();
+            if(!front) {
+                if(!q.empty()) {
+                    q.push(NULL);
+                }
+                continue;
+            }
+            view.push_back(front->data);
+            if(front->left) q.push(front->left);
+            if(front->right) q.push(front->right);
+            while(!q.empty() && q.front()) {
+                front = q.front();
+                q.pop();
+                if(front->left) q.push(front->left);
+                if(front->right) q.push(front->right);
+            }
+        }
+        return view;
     }
-    vector<int> ans;
-    for(auto v: view) {
-        ans.push_back(v.second);
-    }
-    return ans;
-}
+};
