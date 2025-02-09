8e73321f4f84d9427164e3ede354757b885791eb
Shayak <44469461+shayolk@users.noreply.github.com>
2025-02-09 13:32:08 +0000

    https://www.geeksforgeeks.org/problems/topological-sort/1

--- Topological sort.cpp
+++ Topological sort.cpp
@@ -1,30 +1,29 @@
-// The Graph structure is as folows
-
-/*  Function which sorts the graph vertices in topological form
-*   N: number of vertices
-*   adj[]: input graph
-*/
-
-void dfs(int now, vector<int> adj[], vector<bool>& vis, vector<int>& ret) {
-    vis[now]=true;
-    for(int neb: adj[now]) {
-        if(!vis[neb]) {
-            dfs(neb, adj, vis, ret);
+class Solution {
+    int n;
+    vector<bool> vis;
+    vector<int> topo_order;
+    
+    void dfs(int now, vector<vector<int>>& adj) {
+        vis[now] = true;
+        for(int neb: adj[now]) {
+            if(!vis[neb]) {
+                dfs(neb, adj);
+            }
         }
+        topo_order.push_back(now);
     }
-    ret.emplace_back(now);
-}
-
-vector<int> topoSort(int V, vector<int> adj[]) {
-    // Your code here
-    stack<int> s;
-    vector<bool> vis(V);
-    static vector<int> ret;
-    for(int i=0; i<V; ++i) {
-        if(!vis[i]) {
-            dfs(i, adj, vis, ret);
+    
+  public:
+    // Function to return list containing vertices in Topological order.
+    vector<int> topologicalSort(vector<vector<int>>& adj) {
+        n = adj.size();
+        vis.resize(n);
+        for(int i = 0; i < n; ++i) {
+            if(!vis[i]) {
+                dfs(i, adj);
+            }
         }
+        reverse(topo_order.begin(), topo_order.end());
+        return topo_order;
     }
-    reverse(ret.begin(), ret.end());
-    return ret;
-}
+};
