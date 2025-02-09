d5d35d9987e59aabd218d55d49ac35b0dec00cbe
Shayak <44469461+shayolk@users.noreply.github.com>
2025-02-09 19:30:46 +0000

    https://www.geeksforgeeks.org/problems/minimum-spanning-tree/1

--- Minimum Spanning Tree.cpp
+++ Minimum Spanning Tree.cpp
@@ -1,31 +1,35 @@
-class Solution
-{
-	public:
-	//Function to find sum of weights of edges of the Minimum Spanning Tree.
-    int spanningTree(int V, vector<vector<int>> adj[])
-    {
-        int ans=0;
-        vector<int> dist(V, 2e9+5);
-        vector<bool> vis(V);
-        dist[0]=0;
-        priority_queue<pair<int,int>, vector<pair<int,int>>, greater<pair<int,int>>> p;
-        p.push({0,0});
-        while(!p.empty()) {
-            pair<int,int> t=p.top();
-            p.pop();
-            int wt=t.first, now=t.second;
-            if(vis[now]) {
+class Solution {
+    using edge = pair<int,int>;
+    using low_pq_edges = priority_queue<edge,vector<edge>,greater<edge>>;
+
+  public:
+    // Function to find sum of weights of edges of the Minimum Spanning Tree.
+    int spanningTree(int V, vector<vector<int>> adj[]) {
+        vector<int> vis(V);
+        int sum = 0;
+        low_pq_edges pq;
+        
+        auto visitAndAddEdges = [&](int now) {
+            vis[now] = true;
+            for(vector<int> vi: adj[now]) {
+                int neb = vi[0];
+                int cost = vi[1];
+                pq.push({cost, neb});
+            }
+        };
+        
+        visitAndAddEdges(0);
+        while(!pq.empty()) {
+            edge e = pq.top();
+            pq.pop();
+            int cost = e.first;
+            int neb = e.second;
+            if(vis[neb]) {
                 continue;
             }
-            vis[now]=true;
-            ans+=wt;
-            for(auto neb: adj[now]) {
-                if(!vis[neb[0]] && neb[1]<dist[neb[0]]) {
-                    dist[neb[0]]=neb[1];
-                    p.push({neb[1], neb[0]});
-                }
-            }
+            sum += cost;
+            visitAndAddEdges(neb);
         }
-        return ans;
+        return sum;
     }
 };
